fe5327fcf07c0d75f42e579a41c5d9fe41942f8f
ericshortcut <ericshortcut@gmail.com>
2019-11-17 21:58:06 -0300

    Adicionando configuração

--- src/main/java/com/itau/servico/sms/config/KafkaConfiguration.java
+++ src/main/java/com/itau/servico/sms/config/KafkaConfiguration.java
@@ -3,6 +3,7 @@ package com.itau.servico.sms.config;
 import com.itau.servico.sms.model.SMSMessage;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -11,16 +12,21 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
 @EnableKafka
 @Configuration
 public class KafkaConfiguration {
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String BOOTSTRAP_SERVERS_CONFIG;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory(){
         Map<String,Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
@@ -37,7 +43,7 @@ public class KafkaConfiguration {
     public ConsumerFactory<String, SMSMessage> consumerSMSMessageFactory(){
         Map<String,Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
         config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_json");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
--- src/main/java/com/itau/servico/sms/listener/KafkaConsumer.java
+++ src/main/java/com/itau/servico/sms/listener/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.itau.servico.sms.listener;
 
 import com.itau.servico.sms.model.SMSMessage;
 import com.itau.servico.sms.service.TwilioSMS;
+import com.twilio.exception.TwilioException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -16,6 +17,11 @@ public class KafkaConsumer {
 
     @KafkaListener( topics = "${app.topics.sms-consumer}", containerFactory = "kafkaSMSMessageListenerContainerFactory")
     public void consume(SMSMessage smsMessage){
-        twilioSendSMS.send(smsMessage);
+        try {
+            twilioSendSMS.send(smsMessage);
+        } catch (TwilioException e){
+            log.error(e.getMessage());
+            // TODO: Implementar fallback para mensagens que falharem
+        }
     }
 }
